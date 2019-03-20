5a0c58e3e68eaf6e476ed9ce4f2a570b49ba013c
Manik Hosen <rahathossenmanik@gmail.com>
2019-03-20 22:47:16 +0600

    PHP Form Completed.

---
+++ FormValidation.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>  
+<html>
+    <body>  
+        <?php
+            $name = $email = $gender = $comment = $website = "";
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $name = test_input($_POST["name"]);
+                $email = test_input($_POST["email"]);
+                $website = test_input($_POST["website"]);
+                $comment = test_input($_POST["comment"]);
+                $gender = test_input($_POST["gender"]);
+            }
+            function test_input($data) {
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                return $data;
+            }
+            echo "<h2>Your Input:</h2>";
+            echo $name;
+            echo "<br>";
+            echo $email;
+            echo "<br>";
+            echo $website;
+            echo "<br>";
+            echo $comment;
+            echo "<br>";
+            echo $gender;
+        ?>
+    </body>
+</html>
